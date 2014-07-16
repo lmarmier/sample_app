bedbbd82e0d875efc150f69d4f03b7d491408069
Lionel Marmier <lion.mar@me.com>
2014-07-16 14:40:42 +0200

    Fin des pages statiques

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe PagesController, :type => :controller do
+describe PagesController do
+  render_views
 
-  describe "GET 'home'" do
+  describe "GET 'homes'" do
     it "returns http success" do
       get 'home'
       expect(response).to be_success
+    end
+    it "devrait avoir le bon titre" do
+        get 'home'
+        expect(response.body) .to match("Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
@@ -14,6 +19,21 @@ RSpec.describe PagesController, :type => :controller do
       get 'contact'
       expect(response).to be_success
     end
+    it "devrait avoir le bon titre" do
+        get 'contact'
+        expect(response.body) .to match("Simple App du Tutoriel Ruby on Rails | Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+      it "returns http success" do
+          get 'about'
+          expect(response).to be_success
+      end
+      it "devrait avoir le bon titre" do
+          get 'about'
+          expect(response.body) .to match("Simple App du Tutoriel Ruby on Rails | A propos")
+      end
   end
 
 end
